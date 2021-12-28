0ebb3a20a5158d6eaec1778e5abb64c651e58006
Ali Zein Al Dine <alized559@gmail.com>
2021-12-28 22:58:49 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { NewDonorComponent } from './newDonor/newDonor.component';
+import { ContactUsComponent } from './contactUs/contactUs.component';
+import { SearchDonorComponent } from './searchDonor/searchDonor.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'newDonor', component: NewDonorComponent },
+  { path: 'searchDonor', component: SearchDonorComponent },
+  { path: 'contact', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
