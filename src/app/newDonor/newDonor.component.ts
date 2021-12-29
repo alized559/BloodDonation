56ba19c94b4cd9a78c8915a391198622419de7b7
Ali Zein Al Dine <alized559@gmail.com>
2021-12-29 13:19:27 +0200

    .

--- src/app/newDonor/newDonor.component.ts
+++ src/app/newDonor/newDonor.component.ts
@@ -12,31 +12,31 @@ export class NewDonorComponent implements OnInit {
   ngOnInit() {
   }
 
-  // ngAfterViewInit() {
-  //   var button = <HTMLVideoElement> document.getElementById("scroll");
+  ngAfterViewInit() {
+    var button = <HTMLVideoElement> document.getElementById("scroll");
     
-  //   var nav =  <HTMLVideoElement> document.getElementById("nav");
-  //   var navImage = <HTMLVideoElement> document.getElementById("navImg");
+    var nav =  <HTMLVideoElement> document.getElementById("nav");
+    var navImage = <HTMLVideoElement> document.getElementById("navImg");
 
-  //   window.onscroll = function() {
-  //     change();
-  //   };
+    window.onscroll = function() {
+      change();
+    };
     
-  //   function change() {
-  //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  //       button.style.display = "block";
-  //       nav.style.backgroundColor = "#A52A2A";
-  //       navImage.style.width = "30px";
-  //     } else {
-  //       button.style.display = "none";
-  //       nav.style.backgroundColor = "transparent";
-  //       navImage.style.width = "50px";
-  //     }
-  //   }
-  // }
+    function change() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+        nav.style.backgroundColor = "#A52A2A";
+        navImage.style.width = "30px";
+      } else {
+        button.style.display = "none";
+        nav.style.backgroundColor = "transparent";
+        navImage.style.width = "50px";
+      }
+    }
+  }
 
-  // scroll() {
-  //   window.scrollTo({top: 0, behavior: 'smooth'});
-  // }
+  scroll() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 
 }
